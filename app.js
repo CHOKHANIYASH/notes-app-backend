6cb61048071949f13f25c58c70a606b33841a5ae
yash chokhani <yashchokhani95@gmail.com>
2023-02-17 19:12:05 +0530

    changed cookie config

--- app.js
+++ app.js
@@ -58,7 +58,8 @@ const sessionConfig = {
     cookie:{
         httpOnly:true,
         expires: Date.now() + 1000*60*60*24*7,
-        secure: process.env.NODE_ENV === "production",
+        secure: true,
+        sameSite:'none',
         maxAge:1000*60*60*24*7,
         
     }
